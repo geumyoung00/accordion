19d3a43804a0ffce56c6b80d4f8e27b6b1856de6
geumyoung <1gyn093@gmail.com>
2023-09-19 23:30:20 +0900

    Question 컴포넌트 생성

---
+++ src/Question.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Question = () => {
+  return (
+    <section>
+      <div>
+        <h4>제목</h4>
+        <button>+</button>
+      </div>
+      <p>내용</p>
+    </section>
+  );
+};
+
+export default Question;

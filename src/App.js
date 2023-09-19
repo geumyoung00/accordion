1d9e1c5c88a3e415afbd4308d29f7d234e32ffd7
geumyoung <1gyn093@gmail.com>
2023-09-19 23:49:57 +0900

    버튼 클릭시 info on/off 전환, 버튼의 아이콘 +/- 전환

--- src/App.js
+++ src/App.js
@@ -22,16 +22,16 @@ const questions = [
   {
     id: 1,
     title: "Is this a good product?",
-    info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem.",
+    info: "1. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem.",
   },
   {
     id: 2,
     title: "How much does it cost?",
-    info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem.",
+    info: "2. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem.",
   },
   {
     id: 3,
     title: "When can I get it?",
-    info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem.",
+    info: "3. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem.",
   },
 ];
--- src/Question.js
+++ src/Question.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Question = ({ questions }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const openHandler = (e) => {
+    setIsOpen((prev) => !prev);
+  };
   return (
     <section>
       <div>
         <h4>{questions.title}</h4>
-        <button>+</button>
+        <button onClick={openHandler}>{isOpen ? "-" : "+"}</button>
       </div>
-      <p>{questions.info}</p>
+      {isOpen ? <p>{questions.info}</p> : <br />}
     </section>
   );
 };
